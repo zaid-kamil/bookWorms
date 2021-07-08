7dce06749b71f185f82863edd24d6a8c369f12f8
zaid kamil <xaidmetamorphos@gmail.com>
2021-07-08 14:10:47 +0530

    form update

--- app/forms.py
+++ app/forms.py
@@ -1,2 +1,14 @@
+from app.models import Contact, Report
 from django import forms
 from django.forms.fields import IntegerField
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('name', 'email', 'subject', 'messages')
+
+class ReportForm(forms.Modelform):
+    class Meta:
+        model = Report
+        fields = ('feedbackfor', 'message', 'rating')
